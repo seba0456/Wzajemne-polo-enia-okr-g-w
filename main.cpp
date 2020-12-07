f65c767e4ae3885c014c34679873381da745644c
Sebastian <46867564+seba0456@users.noreply.github.com>
2020-12-07 18:41:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,48 +2,55 @@
 using namespace std;
 int main()
 {
-    int instrukcja;
-    double ab;
-    double r1;
-    double r2;
-    cout << "Mam AB, mam r1, mam r2 nie mam nazwy - 1" << endl;
-    cout << "Mam AB, mam r1, nie mam r2  mam nazwe - 2 NIE DZIALA" << endl;
-    cin >> instrukcja;
-   if (instrukcja == 1)
+    int ile;
+    cout << "Ile chcesz wykonac obliczen? ";
+    cin >> ile;
+    for (int i = 1; i <= ile; i++)
     {
-        cout << "Podaj AB:";
-        cin >> ab;
-        cout << "Podaj R1: ";
-        cin >> r1;
-        cout << "Podaj R2; ";
-        cin >> r2;
-        if (ab == 0)
+        int instrukcja;
+        double ab;
+        double r1;
+        double r2;
+        cout << "Mam AB, mam r1, mam r2 nie mam nazwy - 1" << endl;
+        cout << "Mam AB, mam r1, nie mam r2  mam nazwe - 2 NIE DZIALA" << endl;
+        cin >> instrukcja;
+        if (instrukcja == 1)
         {
-            cout << "Okregi wspolsrodkowe" << endl;
+            cout << "Podaj AB:";
+            cin >> ab;
+            cout << "Podaj R1: ";
+            cin >> r1;
+            cout << "Podaj R2; ";
+            cin >> r2;
+            if (ab == 0)
+            {
+                cout << "Okregi wspolsrodkowe" << endl;
+            }
+            else if (r1 - r2 < ab && ab < r1 + r2)
+            {
+                cout << r1 << " - " << r2 << " < " << ab << " < " << r1 << " + " << r2 << " Okregi przecinaja sie." << endl;
+            }
+            else if (ab > r1 + r2)
+            {
+                cout << ab << " > " << r1 << " + " << r2 << " Okregi rozlacznie zewnetrzne." << endl;
+            }
+            else if (ab == r1 + r2)
+            {
+                cout << ab << " = " << r1 << "+" << r2 << " Okregi styczne zewnetrzne." << endl;
+            }
+            else if (ab == r1 - r2)
+            {
+                cout << ab << " = " << r1 << "-" << r2 << " Okregi styczne wewnetrznie." << endl;
+            }
+            else if (ab < r1 - r2)
+            {
+                cout << ab << " < " << r1 << "-" << r2 << " Okregi rozlaczne wewnetrznie." << endl;
+            }
         }
-        else if (r1 - r2 < ab && ab < r1 + r2)
-        {
-            cout << r1 << " - " << r2 << " < " << ab << " < " << r1 << " + " << r2 << " Okregi przecinaja sie." << endl;
-        }
-        else if (ab > r1 + r2)
-        {
-            cout << ab << " > " << r1 << " + " << r2 << " Okregi rozlacznie zewnetrzne." << endl;
-        }
-        else if (ab == r1 + r2)
-        {
-            cout << ab << " = " << r1 << "+" << r2 << " Okregi styczne zewnetrzne." << endl;
-        }
-        else if (ab == r1 - r2)
-        {
-            cout << ab << " = " << r1 << "-" << r2 << " Okregi styczne wewnetrznie." << endl;
-        }
-        else if (ab < r1 - r2)
-        {
-            cout << ab << " < " << r1 << "-" << r2 << " Okregi rozlaczne wewnetrznie." << endl;
-        }
+
+        cin.ignore();
+        cin.get();
     }
-    
-    cin.ignore();
-    cin.get();
+
 }
    
